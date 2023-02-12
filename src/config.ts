3258cefb5ce61d611471fd5bf1aa54428e70e434
Bgomathi <111361765+BalamuruganGomathi@users.noreply.github.com>
2023-02-12 22:55:42 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
-/*export const uri ='https://volvocars--cspdev.sandbox.my.site.com/services/oauth2/authorize?'
+export const uri ='https://volvocars--cspdev.sandbox.my.site.com/services/oauth2/authorize?'
 export const config ={
   client_id:'3MVG904d7VkkD2aNZc_uaegI_igPMOKIVx.gVBOBeCoUSNDz23QBExCUD2OxODw5tgXAFOHc4aQ=='
-  redirect_uri:'https://www.mycustomerorderstatus.com/oauth2/callback'
+  redirect_uri:'https://mbrandt85.github.io/rp_sf_client/oauth2/callback'
   response_type:'code'
-}*/
+}
 
-export const uri = 'https://volvocars--cspdev.sandbox.my.site.com/services/oauth2/token'
+/*export const uri = 'https://volvocars--cspdev.sandbox.my.site.com/services/oauth2/token'
 export const sfToken = '6Cel800D7a00000019bH8887a0000007PGBqDO04vy3HO2zLWgNRnnlQwGKSg9n3LsRfyBJykUnvkfjvIfeJTOlWjGojknMPHc2hiBceFZv'
 export const config = {
   client_id: '3MVG904d7VkkD2aNZc_uaegI_igPMOKIVx.gVBOBeCoUSNDz23QBExCUD2OxODw5tgXAFOHc4aQ==',
   client_secret: '5F0D06499F92B1F83FEAC7022E144B089F62898428DE6A36F7D65D2735BA575D',
   grant_type: 'password'
-}
+}*/
